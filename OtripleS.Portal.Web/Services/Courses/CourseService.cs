cb5368824541f9fa532b3ab6fecdc1d3822e3601
Milan Toncic <milantoncic@outlook.com>
2021-11-14 18:54:29 +0100

    Added Foundation Service GetAllCourses interface and implementation

---
+++ OtripleS.Portal.Web/Services/Courses/CourseService.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using OtripleS.Portal.Web.Brokers.Apis;
+using OtripleS.Portal.Web.Models.Courses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OtripleS.Portal.Web.Services.Courses
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IApiBroker apiBroker;
+
+        public CourseService(IApiBroker apiBroker)
+        {
+            this.apiBroker = apiBroker;
+        }
+
+        public ValueTask<List<Course>> RetrieveAllCoursesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
