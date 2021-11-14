89f2eccd71149488aa0ecc2e93d3803eb4a4a29a
Milan Toncic <milantoncic@outlook.com>
2021-11-14 18:54:40 +0100

    ShouldRetrieveAllCoursesAsync -> FAIL

---
+++ OtripleS.Portal.Web.Tests.Unit/Services/Courses/CourseServiceTests.Logic.cs
@@ -0,0 +1,41 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using OtripleS.Portal.Web.Models.Courses;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Services.Courses
+{
+    public partial class CourseServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllCoursesAsync()
+        {
+            var randomCourses = CreateRandomCourses();
+            var returningCourses = randomCourses;
+            var expectedRetrievedCourses = returningCourses.DeepClone();
+
+            this.apiBrokerMock.Setup(broker => 
+                broker.GetAllCoursesAsync())
+                    .ReturnsAsync(returningCourses);
+
+            List<Course> retrievedCourses = 
+                await this.courseService.RetrieveAllCoursesAsync();
+
+            retrievedCourses.Should().BeEquivalentTo(expectedRetrievedCourses);
+
+            this.apiBrokerMock.Verify(broker => 
+                broker.GetAllCoursesAsync(),
+                    Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ OtripleS.Portal.Web.Tests.Unit/Services/Courses/CourseServiceTests.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Moq;
+using OtripleS.Portal.Web.Brokers.Apis;
+using OtripleS.Portal.Web.Models.Courses;
+using OtripleS.Portal.Web.Services.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Services.Courses
+{
+    public partial class CourseServiceTests
+    {
+        private readonly Mock<IApiBroker> apiBrokerMock;
+        private readonly ICourseService courseService;
+
+        public CourseServiceTests()
+        {
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.courseService = new CourseService(
+                apiBroker: this.apiBrokerMock.Object);
+        }
+
+        private static int CreateRandomNumber() => new IntRange(min: 2, max: 10).GetValue();
+
+        public static List<Course> CreateRandomCourses() =>
+            CreateCourseFiller().Create(count: CreateRandomNumber()).ToList();
+
+        private static Filler<Course> CreateCourseFiller()
+        {
+            var filler = new Filler<Course>();
+
+            filler.Setup()
+                  .OnType<DateTimeOffset>().Use(DateTimeOffset.Now);
+
+            return filler;
+        }
+    }
+}
